0bc0b09ec9892111ce0e03a9f147b9db5aaa9440
Daniel Song <xdtsong@gmail.com>
2017-03-07 19:57:08 -0800

    Add User class based views

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,6 +1,7 @@
 from .models import Snippet
-from .serializers import SnippetSerializer
+from .serializers import SnippetSerializer, UserSerializer
 from rest_framework import generics
+from django.contrib.auth.models import User
 
 
 class SnippetList(generics.ListCreateAPIView):
@@ -11,3 +12,11 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
