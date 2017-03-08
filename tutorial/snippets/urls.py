9a4ff4537836bc377869836efb2e48af458c4c11
Daniel Song <xdtsong@gmail.com>
2017-03-07 20:58:16 -0800

    Add schema to urls.py

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from .views import SnippetViewSet, UserViewSet, api_root
 from rest_framework import renderers
+from rest_framework.schemas import get_schema_view
 
 snippet_list = SnippetViewSet.as_view({
     'get': 'list',
@@ -22,6 +23,7 @@ user_list = UserViewSet.as_view({
 user_detail = UserViewSet.as_view({
     'get': 'retrieve'
 })
+schema_view = get_schema_view(title='Pastebin API')
 
 # API Endpoints
 urlpatterns = [
@@ -31,6 +33,7 @@ urlpatterns = [
     url(r'^snippets/(?P<pk>[0-9]+)/highlight/$', snippet_highlight, name='snippet-highlight'),
     url(r'^users/$', user_list, name='user-list'),
     url(r'^users/(?P<pk>[0-9]+)/$', user_detail, name='user-detail'),
+    url('^schema/$', schema_view),
 ]
 
 # Login and logout views for the browse-able API
