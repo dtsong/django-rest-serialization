b19a669c231013431feae86671a7513ffc22dca1
Daniel Song <xdtsong@gmail.com>
2017-03-07 20:40:55 -0800

    Clean imports in view.py

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -2,7 +2,6 @@ from .models import Snippet
 from .serializers import SnippetSerializer, UserSerializer
 from .permissions import IsOwnerOrReadOnly
 from django.contrib.auth.models import User
-from rest_framework import generics
 from rest_framework import permissions
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
