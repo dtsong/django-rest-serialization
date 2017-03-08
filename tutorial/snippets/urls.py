c1e42de38abdcd2d22c10ff231a9a67c0ee905b2
Daniel Song <xdtsong@gmail.com>
2017-03-07 18:52:15 -0800

    Fix URL routing issue

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -3,8 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-    url(r'^snippets/$', views.JSONResponse.snippet_list),
-    url(r'^snippets/(?P<pk>[0-9]+)/$', views.JSONResponse.snippet_detail),
+    url(r'^snippets/$', views.snippet_list),
+    url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
